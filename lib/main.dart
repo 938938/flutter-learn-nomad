36e1a6d3b796d6ded9aab931542e184e031873c2
938938 <938kjh@gmail.com>
2025-09-02 22:11:22 +0900

    [2.3] Hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
+}
+
+// build 메소드는 widget의 UI를 생성
+class App extends StatelessWidget {
+  @override // 부모 class에 이미 있는 메소드를 override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello!')),
+        body: Center(child: Text('Hello world!')),
+      ),
+    );
+  }
 }
